b0f83df22a684c97357189e55f7b687240f6569e
Sébastien Gonzalez <>
2020-03-27 15:24:28 +0100

    Connexion admin fonctionnelle

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -18,11 +18,40 @@ export class UserService {
   signIn(isAdmin: boolean, email: string, password: string) {
     console.log('test co 2');
     this.isAdmin = isAdmin;
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+    const body = '{ "mail" : "' + email + '", "password" : "' + password + '"}';
     return new Promise(
       (resolve, reject) => {
         if (isAdmin) {
           console.log('test co 3');
-          this.signInAdmin(email, password);
+          const url  = 'http://localhost:4200/RandoExpress_API/ws/rest/authAdmin';
+          this.httpClient
+            .post(url, body, httpOptions)
+            .subscribe(
+              (response) => {
+                const responseJSON = JSON.stringify(response);
+                const json = JSON.parse(responseJSON);
+
+                console.log(json.message)
+                if (json.message === 'Connexion validé') {
+                  console.log('ok')
+                  this.jwt = json.jwt;
+                  resolve();
+                } else {
+                  console.log('pas ok')
+                  reject('Connexion refusé');
+                }
+              },
+              (error) => {
+                console.log('Erreur ! : ' + error);
+              }
+            );
+        } else { // ajout connexion client
         }
       }
     );
@@ -43,9 +72,17 @@ export class UserService {
       .subscribe(
         (response) => {
           const responseJSON = JSON.stringify(response);
+          const json = JSON.parse(responseJSON);
 
-          console.log(responseJSON);
-          console.log(responseJSON[0]['message'] + ' ' + response[1]);
+          console.log(json.message)
+          if (json.message === 'Connexion validé') {
+            console.log('ok')
+            this.jwt = json.jwt;
+            return true;
+          } else {
+            console.log('pas ok')
+            return false;
+          }
         },
         (error) => {
           console.log('Erreur ! : ' + error);
